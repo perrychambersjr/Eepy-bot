7366088f79cc225d43531fa1db46f7638c03fa67
blake <bmacreasser@gmail.com>
2025-08-09 16:47:16 -0500

    minor formatting and remove unused code

--- commands/general/createAward.js
+++ commands/general/createAward.js
@@ -1,4 +1,11 @@
-const { SlashCommandBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, ComponentType, ModalBuilder, TextInputBuilder, TextInputStyle, time } = require('discord.js')
+const { SlashCommandBuilder, 
+    ActionRowBuilder, 
+    ButtonBuilder, 
+    ButtonStyle, 
+    ComponentType, 
+    ModalBuilder, 
+    TextInputBuilder, 
+    TextInputStyle } = require('discord.js')
 
 const categories = ['Funny', 'Performance', 'Personality', 'Other']
 
@@ -10,6 +17,7 @@ module.exports = {
 
         const row = new ActionRowBuilder()
 
+        //add each category button
         categories.forEach(cat => {
              let categoryButton = new ButtonBuilder()
                  .setCustomId(cat)
@@ -65,100 +73,6 @@ module.exports = {
                 });
             }
         })
-
-        // let buttonClick
-        // try {
-        //     buttonClick = msg.awaitMessageComponent({
-        //         filter: i => categories.includes(i.customId) && i.user.id === interaction.user.id,
-        //         componentType: ComponentType.Button,
-        //         time: 30000
-        //     })
-        // } catch {
-        //     return interaction.followUp({content: 'No category selected in time.'})
-        // }
-
-        // const chosenCategory = buttonClick.customId
-
-        // const modal = new ModalBuilder()
-        //     .setCustomId(`awardModal-${chosenCategory}`)
-        //     .setTitle('Enter Award Title')
-
-        // const awardInput = new TextInputBuilder()
-        //     .setCustomId('awardTitle')
-        //     .setLabel('Award Title')
-        //     .setStyle(TextInputStyle.Short)
-        //     .setPlaceholder('e.g, Biggest Pumper')
-        //     .setRequired(true)
-
-        // const actionRow = new ActionRowBuilder().addComponents(awardInput)
-        // modal.addComponents(actionRow)
-
-        // await buttonClick.showModal(modal)
-
-        // try {
-        //     const modalSubmit = await buttonClick.awaitModalSubmit({
-        //         filter: i => i.user.id === interaction.user.id,
-        //         time: 60000
-        //     })
-
-        //     const awardTitle = modalSubmit.fields.getTextInputValue('awardTitle')
-
-        //     await modalSubmit.reply({
-        //         content: `Award saved!\nCategory: ${chosenCategory}\nAward: ${awardTitle}`
-        //     })
-        // } catch {
-        //     return interaction.followUp({content: 'Award title not entered in time.'})
-        // }
-
-        // const row = new ActionRowBuilder()
-
-        // categories.forEach(cat => {
-        //     let categoryButton = new ButtonBuilder()
-        //         .setCustomId(cat)
-        //         .setLabel(cat)
-        //         .setStyle(ButtonStyle.Primary)
-                
-        //     row.addComponents(categoryButton)
-        // })
-
-        // await interaction.reply({
-        //     content: 'Select a category',
-        //     components: [row],
-        // })
-
-        // const buttonClick = await interaction.channel.awaitMessageComponent({
-        //     filter: i => categories.includes(i.customId),
-        //     componentType: ComponentType.Button,
-        //     time: 30000
-        // }).catch(() => null)
-
-        // if (!buttonClick) return interaction.followUp({content: 'No category selected.'})
-
-        // const chosenCategory = buttonClick.customId
-
-        // await buttonClick.update({ 
-        //     content: `You selected ${chosenCategory}.\nPlease type the award in chat:`,
-        //     components: []
-        // })
-
-
-
-
-        // const collectedMsg = await interaction.channel.awaitMessages({
-        //     filter: m => m.author.id === interaction.user.id,
-        //     max: 1,
-        //     time: 60000
-        // })
-
-        // if (!collectedMsg.size){
-        //     return interaction.followUp({ content: 'Award was not entered in time.'})
-        // }
-
-        // const awardTitle = collectedMsg.first().content
-
-        // await interaction.followUp({
-        //     content: `Award saved!\nCategory: ${chosenCategory}\nAward: ${awardTitle}`
-        // })
 
     }
 }
